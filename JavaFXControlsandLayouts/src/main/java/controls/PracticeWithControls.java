ee7f5bfab39d6f0f0a3f904a1a763ed058121583
Nate Hascup <nshascup@gmail.com>
2018-01-18 13:42:15 -0800

    Committing work done in first part of video

--- JavaFXControlsandLayouts/src/main/java/controls/PracticeWithControls.java
+++ JavaFXControlsandLayouts/src/main/java/controls/PracticeWithControls.java
@@ -1,14 +1,32 @@
 package controls;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextBuilder;
 import javafx.stage.Stage;
+
+import java.util.Random;
 
 public class PracticeWithControls extends Application
 {
     @Override
     public void start(Stage stage) throws Exception
     {
+        //stage.setScene(createButtons());
+        stage.setScene(createTextElements());
         stage.setTitle("Practicing with controls");
         stage.show();
     }
@@ -16,13 +34,87 @@ public class PracticeWithControls extends Application
     //buttons
     public Scene createButtons()
     {
-        return null;
+        //crating a new button
+        Button button = new Button("Click me!");
+        button.setPrefHeight(30);
+        button.setPrefWidth(300);
+        button.setAlignment(Pos.BOTTOM_RIGHT);
+
+        //this is out layout for controls
+        VBox box = new VBox();
+        box.getChildren().add(button);
+
+        //set the spacing with my layout
+        box.setAlignment(Pos.CENTER);
+        box.setPadding(new Insets(20));
+
+        //assign an event handler
+        Random random = new Random();
+        button.setOnAction(new EventHandler<ActionEvent>()
+        {
+
+            @Override
+            public void handle(ActionEvent event)
+            {
+                button.setText("you clicked, here is a random #: " + random.nextInt(100));
+
+            }
+        });
+
+        return new Scene(box, 500, 500);
     }
 
     //text elements - Text, Label, TextField, TextArea
     public Scene createTextElements()
     {
-        return null;
+        VBox verticalStack = new VBox();
+        verticalStack.setAlignment(Pos.CENTER);
+        verticalStack.setPadding(new Insets(10));
+        verticalStack.setSpacing(10);
+
+        //add a banner using the Text class
+        /*Text banner = TextBuilder.create()
+                .text("Enter information")
+                .font(Font.font("Vani", FontWeight.BOLD, 30))
+                .underline(true).build();*/
+
+        Text banner = new Text();
+        banner.setText("Enter information");
+        banner.setFont(Font.font("Vani", FontWeight.BOLD, 30));
+        banner.setUnderline(true);
+
+        verticalStack.getChildren().add(banner);
+
+        //lets add a few label/entry fields
+        Label nameLabel = new Label("Name: ");
+        nameLabel.setAlignment(Pos.CENTER);
+        nameLabel.setPrefWidth(50);
+
+        TextField nameEntry = new TextField();
+        nameEntry.setPrefWidth(200);
+
+        //group together my controls and add them to a parent layout
+        HBox horizontalStack = new HBox();
+        horizontalStack.setAlignment(Pos.CENTER);
+        horizontalStack.getChildren().addAll(nameLabel, nameEntry);
+        verticalStack.getChildren().add(horizontalStack);
+
+        //lets add a few label/entry fields
+        Label bioLabel = new Label("Bio: ");
+        bioLabel.setAlignment(Pos.CENTER_LEFT);
+        bioLabel.setPrefWidth(50);
+
+        TextArea bioEntry = new TextArea();
+        bioEntry.setPrefWidth(200);
+        bioEntry.setWrapText(true);
+
+        //group together my controls and add them to a parent layout
+        horizontalStack = new HBox();
+        horizontalStack.setAlignment(Pos.CENTER);
+        horizontalStack.getChildren().addAll(bioLabel, bioEntry);
+        verticalStack.getChildren().add(horizontalStack);
+
+        return new Scene(verticalStack, 500, 500);
     }
 
     //checkboxes, radio buttons
