3977dbdacdf7f6e707a48044e6085775ad7381c9
Nate Hascup <nshascup@gmail.com>
2018-03-21 14:15:10 -0700

    Added functions in circle adapter

--- DoodlePadApplication/DoodlePadApplication/src/main/java/adapters/CircleAdapter.java
+++ DoodlePadApplication/DoodlePadApplication/src/main/java/adapters/CircleAdapter.java
@@ -8,22 +8,38 @@ import shapes.Circle;
 public class CircleAdapter implements IShape
 {
     private Circle round;
+    private boolean circleFilled;
 
     public CircleAdapter(Circle circle) { this.round = circle; }
 
     @Override
-    public IShape setThickness(double value) {
-        return null;
+    public IShape setThickness(double value)
+    {
+        round.setThickness(value);
+
+        return this;
     }
 
     @Override
-    public IShape setColor(Color value) {
-        return null;
+    public IShape setColor(Color value)
+    {
+        Circle sphere = new Circle(round.getRadius(), round.getX(), round.getY(), round.getThickness(), value);
+
+        round = sphere;
+
+        return this;
     }
 
     @Override
-    public IShape setFilled(boolean value) {
-        return null;
+    public IShape setFilled(boolean value)
+    {
+        Circle sphere = new Circle(round.getRadius(), round.getX(), round.getY(), round.getThickness(), round.getColor());
+
+        round = sphere;
+
+        circleFilled = value;
+
+        return this;
     }
 
     @Override
